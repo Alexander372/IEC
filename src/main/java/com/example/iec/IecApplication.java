bff560959f2668e159a2a744334bccab1219670e
Alexander372 <AlKharj4>
2022-06-06 17:43:31 +0300

    initial stage

---
+++ src/main/java/com/example/iec/IecApplication.java
@@ -0,0 +1,13 @@
+package com.example.iec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IecApplication.class, args);
+    }
+
+}
